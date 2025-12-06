26a11fdf76cf5325b9c06eb0e5c63a4e964d7b87
fidelisdaleck <fidelisdaleck386@gmail.com>
2025-12-06 02:05:55 +0100

    Implement reveal on scroll functionality
    Add scroll event listener to reveal elements on scroll.

--- TP-Web/main.js
+++ TP-Web/main.js
@@ -1,1 +1,18 @@
+<script>
+    const reveals = document.querySelectorAll('.reveal');
 
+    function revealElements() {
+      for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const revealTop = reveals[i].getBoundingClientRect().top;
+        const revealPoint = 100;
+
+        if (revealTop < windowHeight - revealPoint) {
+          reveals[i].classList.add('active');
+        }
+      }
+    }
+
+      window.addEventListener('scroll', revealElements);
+      revealElements();
+  </script>
